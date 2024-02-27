ff804d693a15609ff08ece92e7e0235361edcb01
Dario Pešerović <d.peserovic@gmail.com>
2024-02-27 10:46:34 +0100

    add new btns

--- src/App.js
+++ src/App.js
@@ -1,34 +1,70 @@
-import { useState } from "react";
-
-const setCookie = () => {
-  fetch('https://fancy-large-scale.glitch.me/set-cookie', { method: "GET", credentials: 'include' })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text(); // or response.json() if expecting JSON data
-    })
-    .then(data => {
-      console.log('Response:', data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
-
 function App() {
-  const [text, setText] = useState("");
+  const setCookie = () => {
+    fetch("https://fancy-large-scale.glitch.me/set-cookie", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log("Response:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" value={text} onChange={e => setText(e.target.value)} />
-        <input type="submit" onClick={e => {
-          e.preventDefault();
-          setCookie();
-        }} />
-      </form>
-      <p>{text}</p>
-      <iframe src="https://fancy-large-scale.glitch.me/"></iframe>
+      <div>
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            console.log(await document.hasStorageAccess());
+          }}
+        >
+          document.hasStorageAccess()
+        </button>
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            console.log(
+              await navigator.permissions.query({ name: "storage-access" })
+            );
+          }}
+        >
+          permission
+        </button>
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            console.log(await document.requestStorageAccess());
+          }}
+        >
+          document.requestStorageAccess()
+        </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setCookie();
+          }}
+        >
+          /set-cookie
+        </button>
+      </div>
+      <br />
+      <div>
+        <iframe
+          sandbox="allow-storage-access-by-user-activation
+                  allow-scripts
+                  allow-same-origin"
+          src="https://fancy-large-scale.glitch.me/"
+        />
+      </div>
     </div>
   );
 }
